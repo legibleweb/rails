12633fdd750157c3dec5d65524796bc2ecf2c9b5
Brad Gessler <bradgessler@gmail.com>
2023-10-18 16:35:10 -0700

    Add header to resource

--- app/content/pages/index.md
+++ app/content/pages/index.md
@@ -3,3 +3,9 @@
 Tired of how much crap is on the Internet? Let's fix it and build an internet that disincentivizes "influencers" and advertisers to the max, but delivers a fast, amazing reading experience.
 
 Legible Web is built on top of existing standards, like HTTP 1.1, and passes on features that have become too complex, like "modern" HTML, JavaScript, and CSS.
+
+## How it works
+
+It's pretty simple really. Request a resource with the `Accept: text/markdown` header and a markdown file gets served up for the client to render. That's it! That's the technology.
+
+Ok ok ... there's one more thing: navigation. It's a JSON file, which you can read more about at [Navigation](/navigation) or request the file via `Accept: application/json`.
--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,48 +1,46 @@
 class SiteController < ApplicationController
   def show
-    resource = Sitepress.site.get(request.path)
-
     respond_to do |format|
-      format.all { render_resource }
+      response.header["Navigation-Location"] = url_for
+      format.all  { render_resource }
       format.json { render_navigation  }
       format.html { render_resource type: :md, layout: "application" }
     end
   end
 
   protected
+    Node = Data.define(:title, :url, :children)
 
-  Node = Data.define(:title, :url, :children)
-
-  def current_resource
-    Sitepress.site.get(request.path)
-  end
-
-  def render_navigation
-    render json: navigation(markdown_resource)
-  end
-
-  def markdown_resource
-    json_resource = Sitepress.site.get(request.path)
-    json_resource.node.resources.format(:md)
-  end
-
-  def navigation(resource)
-    Node.new(title: resource.node.name, url: resource.request_path, children: resource.children.map { navigation(_1) })
-  end
-
-  def render_resource(resource = current_resource, type: nil, **)
-    type ||= resource.handler unless resource.handler == Sitepress::Node::DEFAULT_FORMAT
-
-    if resource.renderable?
-      render inline: resource.body, type: type, **
-    else
-      send_binary_resource resource
+    def current_resource
+      Sitepress.site.get(request.path)
     end
-  end
 
-  def send_binary_resource(resource)
-    send_file resource.asset.path,
-      disposition: :inline,
-      type: resource.mime_type.to_s
-  end
+    def render_navigation
+      render json: navigation(markdown_resource)
+    end
+
+    def markdown_resource
+      json_resource = Sitepress.site.get(request.path)
+      json_resource.node.resources.format(:md)
+    end
+
+    def navigation(resource)
+      Node.new(title: resource.node.name, url: resource.request_path, children: resource.children.map { navigation(_1) })
+    end
+
+    def render_resource(resource = current_resource, type: nil, **)
+      type ||= resource.handler unless resource.handler == Sitepress::Node::DEFAULT_FORMAT
+
+      if resource.renderable?
+        render inline: resource.body, type: type, **
+      else
+        send_binary_resource resource
+      end
+    end
+
+    def send_binary_resource(resource)
+      send_file resource.asset.path,
+        disposition: :inline,
+        type: resource.mime_type.to_s
+    end
 end
